0d4f0cbd7cbac2d1f9a8b466324878b0447f0503
Panda-Abhisek <abhisekpanda114@gmail.com>
2025-12-30 15:51:06 +0530

    tweak: Transition from Notification Service to Spring Cloud Stream

--- notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -1,27 +1,37 @@
 package com.ecommerce.notification;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.function.Consumer;
 
+@Slf4j
 @Service
 public class OrderEventConsumer {
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
-        System.out.println("Received Order Event: " + orderEvent);
+//    @RabbitListener(queues = "${rabbitmq.queue.name}")
+//    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
+//        System.out.println("Received Order Event: " + orderEvent);
+//
+//        long orderId = orderEvent.getOrderId();
+//        OrderStatus orderStatus = orderEvent.getStatus();
+//
+//        System.out.println("Order ID: " + orderId);
+//        System.out.println("Order Status: " + orderStatus);
+//
+//        //More Applications of Notification Service -
+//        // Update Database
+//        // Send Notification
+//        // Send Emails
+//        // Generate Invoices
+//        // Send Seller Notifications
+//    }
 
-        long orderId = orderEvent.getOrderId();
-        OrderStatus orderStatus = orderEvent.getStatus();
-
-        System.out.println("Order ID: " + orderId);
-        System.out.println("Order Status: " + orderStatus);
-
-        //More Applications of Notification Service -
-        // Update Database
-        // Send Notification
-        // Send Emails
-        // Generate Invoices
-        // Send Seller Notifications
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated() {
+        return event -> {
+            log.info("Received order created event for order: {}", event.getOrderId());
+            log.info("Received order created event for user id: {}", event.getUserId());
+        };
     }
 }
